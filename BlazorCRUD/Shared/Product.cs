b1e48fbe06e145235f6be18964e094e2a74460d3
T3NaCitY <149932781+T3NaCitY@users.noreply.github.com>
2023-11-14 08:34:37 +0100

    Add project files.

---
+++ BlazorCRUD/Shared/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public String Title { get; set; } = string.Empty;
+        public String Description { get; set; } = string.Empty;
+        public string ImageURL { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
